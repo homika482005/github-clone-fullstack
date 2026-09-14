bacd2a1cbbae66c613bf909ce6d39237c99f34c9
Homika Sirsate <sirsatehomika@gmail.com>
2026-09-14 22:36:22 +0530

    Create notificationModel.js

---
+++ backend/models/notificationModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: { type: String, enum: ["follow", "star", "issue", "pr", "merge"], required: true },
+    message: { type: String, required: true },
+    isRead: { type: Boolean, default: false },
+    link: { type: String, default: "/" } // URL to redirect to when clicked
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+module.exports = Notification;
